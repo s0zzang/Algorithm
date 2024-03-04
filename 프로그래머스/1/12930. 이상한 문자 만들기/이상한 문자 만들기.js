538f6f42ebf608a29e9a46fe43add0da39044b2e
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-03-04 10:20:39 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,4 +1,3 @@
 function solution(s) {
-    const arr = s.split(' ').map(word => [...word].map((t,i) => i%2 ? t.toLowerCase() : t.toUpperCase()) )
-    return arr.join(' ').replaceAll(',','')
+    return s.split(' ').map(word => [...word].map((t,i) => i%2 ? t.toLowerCase() : t.toUpperCase()).join('')).join(' ')
 }
