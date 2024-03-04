04ca9bc328a96838ae2b5fd73b9ce326b497a31c
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-03-04 23:55:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,5 @@
+function solution(s) {
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];    
+    for(let i=0; i<arr.length; i++) s = s.replaceAll(arr[i], i)
+    return +s
+}
