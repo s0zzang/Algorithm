a5b4f8db7d6a7ae4f81ae181b5242b2ab9927cad
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-09-26 18:01:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.09 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 69.6 MB, 시간: 66.69 ms
+메모리: 57.8 MB, 시간: 54.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 17:51:56
+2024년 09월 26일 18:01:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,6 @@
 function solution(participant, completion) {
     const map = new Map()
-    for(let person of completion){
-        if(map.has(person)) map.set(person, map.get(person)+1);
-        else map.set(person, 1)
-    }
+    for(let person of completion) map.set(person, map.has(person) ? map.get(person)+1 : 1)
     for(let person of participant){
         if(map.has(person)){ 
             map.set(person, map.get(person)-1);
