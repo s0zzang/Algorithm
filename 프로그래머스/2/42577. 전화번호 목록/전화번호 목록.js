c6a8e811bf7ce84bf9bf4279b48bcc591cf26ddf
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-10-07 11:07:00 +0900

    [level 2] Title: 전화번호 목록, Time: 823.99 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,16 @@
-function solution(phone_book) {    
-    const sorted = phone_book.sort()    
-    for(let i=0; i<phone_book.length; i++){
-        if(i === phone_book.length-1) return true
-        if(sorted[i+1].startsWith(sorted[i])) return false
+function solution(phoneBook) {
+    let hashMap = {};
+    for (let phone of phoneBook) hashMap[phone] = true;
+    for (let phone of phoneBook) {
+        let prefix = "";
+        for (let char of phone) {
+            prefix += char;
+            // 접두어가 해시맵에 존재하고, 접두어가 자신이 아닌 다른 번호라면 false 반환
+            if (prefix in hashMap && prefix !== phone) {
+                return false;
+            }
+        }
     }
+
+    return true;
 }
