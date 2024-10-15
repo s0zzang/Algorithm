9eff5831a66e335961e372e1dcc251f7c03686c4
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-10-15 18:24:43 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,9 +1,3 @@
 function solution(A, B) {
-    const arr = [...A]
-    let result = 0;
-    for(let i=0; i<arr.length; i++){
-        if(arr.join('') === B) return i
-        arr.unshift(arr.pop())
-    }
-    return -1
+    return (B+B).indexOf(A)
 }
