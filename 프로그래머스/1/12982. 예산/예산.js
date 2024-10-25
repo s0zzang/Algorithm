bd1127f648af6c311fc6cb24996031ef3fe7a259
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-10-25 17:37:41 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,9 +1,3 @@
 function solution(d, budget) {
-    d.sort((a,b) => b-a)
-    let result = 0;
-    while(budget >= d[d.length-1]){
-        budget -= d.pop();
-        result ++;
-    }
-    return result
+    return d.sort((a,b) => a-b).reduce((count, price) => count + ((budget -= price) >= 0), 0)
 }
