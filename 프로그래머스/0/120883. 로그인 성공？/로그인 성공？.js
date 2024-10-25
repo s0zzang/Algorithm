6f96aa70c99b35ed0b3f75bf8a1fb9f02e806d28
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-10-25 16:53:44 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,4 @@
 function solution(id_pw, db) {
-    const map = new Map()
-    for([id,pw] of db) map.set(id, pw)
-    if(map.get(id_pw[0])){
-        if(map.get(id_pw[0]) === id_pw[1]) return 'login'
-        else return "wrong pw"
-    } else return 'fail'
+    const map = new Map(db)
+    return map.has(id_pw[0]) ? map.get(id_pw[0]) === id_pw[1] ? 'login' : 'wrong pw' : 'fail'
 }
