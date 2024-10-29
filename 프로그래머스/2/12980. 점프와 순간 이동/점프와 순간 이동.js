213564da9c612a345cb7cbf1e613ce9575bb442d
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-10-29 12:09:31 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,10 +1,3 @@
 function solution(n){
-    let battery = 1;
-    while(n > 1){
-        if(n%2) {
-            battery += 1;
-            n -= 1;
-        } else n /= 2
-    }
-    return battery
+    return [...n.toString(2)].filter(n => n === '1').length
 }
