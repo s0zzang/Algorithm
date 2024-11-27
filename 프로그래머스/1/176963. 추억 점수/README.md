5bfcfe80bc722357ce738e67926e1888aa450b09
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-11-27 19:48:17 +0900

    [level 1] Title: 추억 점수, Time: 26.12 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/README.md
+++ 프로그래머스/1/176963. 추억 점수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 추억 점수 - 176963 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 31.58 ms
+메모리: 36.8 MB, 시간: 26.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 27일 19:41:05
+2024년 11월 27일 19:48:15
 
 ### 문제 설명
 
--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,13 +1,3 @@
 function solution(name, yearning, photo) {
-    const map = new Map()
-    name.map((v, i) => map.set(v, yearning[i]))
-    
-    let result = Array(photo.length).fill(0);
-    for(let i=0; i<photo.length; i++){
-        for(let j=0; j<photo[i].length; j++){
-            result[i] += map.get(photo[i][j]) || 0;
-        }
-    }
-    
-    return result
+    return photo.map(value => value.reduce((a,c) => a += yearning[name.indexOf(c)] || 0 , 0))
 }
