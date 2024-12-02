93b173fd1b5189235bcb45d80133aeb0c5d387e2
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-12-02 15:56:48 +0900

    [level 2] Title: 구명보트, Time: 39.08 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,17 +1,14 @@
 function solution(people, limit) {
-    people.sort((a, b) => a - b); // 몸무게 순으로 정렬
-    let left = 0; // 가장 가벼운 사람
-    let right = people.length - 1; // 가장 무거운 사람
-    let boats = 0; // 필요한 보트 수
-
-    while (left <= right) {
-        // 가장 무거운 사람과 가장 가벼운 사람의 합이 제한 이하면 같이 태움
-        if (people[left] + people[right] <= limit) {
-            left++; // 가벼운 사람 제외
-        }
-        right--; // 무거운 사람은 항상 제외
-        boats++; // 보트 한 대 추가
+    people.sort((a,b) => a-b)
+    let left = 0;
+    let right = people.length-1;
+    let result = 0;
+    
+    while(left <= right){
+        if(people[left] + people[right] <= limit) left++;
+        right--;
+        result++;
     }
-
-    return boats;
+    
+    return result
 }
