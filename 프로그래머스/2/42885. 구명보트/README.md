0bccb1af3d9f9ffd57b0396f84d7e85057c51a94
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-12-02 15:57:38 +0900

    [level 2] Title: 구명보트, Time: 39.08 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,6 +1,6 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 15:56:46
+2024년 12월 02일 15:57:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,10 +1,29 @@
 function solution(people, limit) {
+    // 정렬 후 투 포인터 설정
+    // Left, right 비교하며 limit을 넘지 않는지 확인
+    // [50, 50, 70, 80] 
+    
+    /*
+    left:people[0] => 50 
+    right:people.length-1 => 80
+    right:people.length-2 => 70
+    right:people.length-3 => 50
+        count++
+    
+    left:people[1] => 50 (이미 찾은걸 어떻게 구분하지)
+    
+    */
+    // left:50 right:people.length-1 => 80
+    // 
+    
     people.sort((a,b) => a-b)
     let left = 0;
     let right = people.length-1;
     let result = 0;
+    console.log(people)
     
     while(left <= right){
+        console.log(people[left] , people[right])
         if(people[left] + people[right] <= limit) left++;
         right--;
         result++;
