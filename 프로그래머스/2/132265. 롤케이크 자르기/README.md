2b395c172565a138020ff97cdef8859567eaa989
이소정 <109408216+s0zzang@users.noreply.github.com>
2024-12-16 19:36:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 120.85 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=javascript) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 129.67 ms
+메모리: 71.5 MB, 시간: 120.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 16일 18:07:55
+2024년 12월 16일 19:36:51
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -4,18 +4,16 @@ function solution(topping) {
     const right = new Map()
     
     for(let i=1; i<topping.length; i++){
-        right.set(topping[i], right.get(topping[i])+1 || 1)
+        right.set(topping[i], (right.get(topping[i]) || 0) + 1)
     }
     
     for(let i=1; i<topping.length; i++){
-        left.set(topping[i], left.get(topping[i]) ? left.get(topping[i])+1 : 1)
-        if(right.get(topping[i]) === 1) {
-            right.delete(topping[i])
-        } else {
-            right.set(topping[i], right.get(topping[i]) -1)             
-        }
+        left.set(topping[i], (left.get(topping[i]) || 0) + 1)
+        if(right.get(topping[i]) === 1) right.delete(topping[i])
+        else right.set(topping[i], right.get(topping[i]) -1)              
         
         if(left.size === right.size) result += 1
     }
+    
     return result
 }
