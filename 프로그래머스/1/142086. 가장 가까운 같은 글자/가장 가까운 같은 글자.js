cf8715920b944b78ed8bb5d0cfdec3a3965f68a4
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-01-28 16:08:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.37 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,20 +1,9 @@
 function solution(s) {
-    const res = [];
+    const lastSeen = {}
     
-    for(let i=s.length-1; i>0; i--){
-        let pointer = i-1;
-        
-        while(pointer > -1){
-            if(s[i] === s[pointer]) {
-                res.push(i-pointer)
-                break
-            } else {
-                if(pointer === 0) res.push(-1)
-                pointer--;
-            }
-        }
-    }
-    
-    res.push(-1)
-    return res.reverse()
+    return [...s].map((s,i) => {
+        const pointer = lastSeen[s] !== undefined ? i-lastSeen[s] : -1;
+        lastSeen[s] = i
+        return pointer
+    })
 }
