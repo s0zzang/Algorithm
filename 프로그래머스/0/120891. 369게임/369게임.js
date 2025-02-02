ed2cb9ff8c321aec06668c0db99ef89004ffa885
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-02-02 23:30:38 +0900

    [level 0] Title: 369게임, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,6 +1,12 @@
 function solution(order) {
-    return [...`${order}`].reduce((a,c) => {
-        if(!(+c % 3) && c > 0) a++;
-        return a;
-    }, 0)
+    // return [...`${order}`].reduce((a,c) => {
+    //     if(!(+c % 3) && c > 0) a++;
+    //     return a;
+    // }, 0)
+    
+    // matchAll : iterator 객체 반환
+    return [...(''+order).matchAll(/[369]/g)].length
+    
+    // match : 배열 반환
+    // return (''+order).match(/[369]/g).length
 }
