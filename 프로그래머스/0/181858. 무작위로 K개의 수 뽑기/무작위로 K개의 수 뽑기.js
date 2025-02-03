2da8fb77a052256af9e2f338b60bb0021fc7ea10
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-02-03 15:48:26 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2.07 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,4 +1,6 @@
 function solution(arr, k) {
-    const set = [...new Set(arr)]
-    return set.length > k ? set.slice(0, k) : [...set, ...Array.from({length: k-set.length}).fill(-1)]
+    const set = new Set(arr)
+    return set.size > k 
+        ? [...set].slice(0, k) 
+        : [...set, ...Array.from({length: k-set.size}).fill(-1)]
 }
