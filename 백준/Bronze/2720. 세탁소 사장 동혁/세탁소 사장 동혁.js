6c4e0991e182d052c1994c2b70b3fd5a872abb0d
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-04-17 17:13:24 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 108 ms, Memory: 10192 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
@@ -2,26 +2,16 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [num, ...arrs] = input;
-const [q, d, n, p] = [25, 10, 5, 1];
+const coins = [25, 10, 5, 1];
 
 for (let i = 0; i < num; i++) {
-  let sum = arrs[i];
-  let [qq, dd, cc, pp] = [0, 0, 0, 0];
-  while (sum >= q) {
-    sum -= q;
-    qq++;
-  }
-  while (sum >= d) {
-    sum -= d;
-    dd++;
-  }
-  while (sum >= n) {
-    sum -= n;
-    cc++;
-  }
-  while (sum >= p) {
-    sum -= p;
-    pp++;
-  }
-  console.log(qq, dd, cc, pp);
+  let amount = +arrs[i];
+
+  const result = coins.map((coin) => {
+    const count = Math.floor(amount / coin);
+    amount %= coin;
+    return count;
+  });
+
+  console.log(result.join(" "));
 }
