e98304b311650ecda58597700b612dbc4b6b9ff4
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-05-22 22:08:40 +0900

    [level 1] Title: 모의고사, Time: 1.60 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,20 +3,16 @@ function solution(answers) {
     const n2 = [2, 1, 2, 3, 2, 4, 2, 5]
     const n3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    const correct1 = answers.map((a, i) => a === n1[(i+5)%5]).reduce((a,c) => a+c, 0)
-    const correct2 = answers.map((a, i) => a === n2[(i+8)%8]).reduce((a,c) => a+c, 0)
-    const correct3 = answers.map((a, i) => a === n3[(i+10)%10]).reduce((a,c) => a+c, 0)
+    const correct1 = answers.filter((a, i) => a === n1[(i+5)%5]).length
+    const correct2 = answers.filter((a, i) => a === n2[(i+8)%8]).length
+    const correct3 = answers.filter((a, i) => a === n3[(i+10)%10]).length
     
-    const list = [correct1, correct2, correct3]
-    const max = Math.max(...list)
-    const map = new Map()
-    for(let i=0; i<3; i++) map.set(i+1, list[i])
+    const max = Math.max(correct1, correct2, correct3)
+    const result = []
     
-    const result = [...map].reduce((acc, [k,v]) => {
-        console.log(v === max, acc)
-        if(v === max) acc.push(k)
-        return acc
-    }, [])
+    if(correct1 === max) result.push(1)
+    if(correct2 === max) result.push(2)
+    if(correct3 === max) result.push(3)
     
     return result
 }
