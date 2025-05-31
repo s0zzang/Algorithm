82555a693bfdcde1fe89d2016a1ded7dfad94740
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-05-31 22:25:40 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.62 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,5 +1,3 @@
 function solution(arr, idx) {
-    arr.splice(0, idx)
-    const index = arr.findIndex(el => el === 1)
-    return index !== -1 ? index+idx : index ;
+    return arr.findIndex((el,i) => i>=idx && el === 1)
 }
