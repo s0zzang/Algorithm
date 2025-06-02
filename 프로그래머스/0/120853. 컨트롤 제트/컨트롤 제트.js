3e3d02f8a9313d9e58cd093c683267458562a316
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-06-02 16:23:52 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,10 +1,8 @@
 function solution(s) {
-    const arr = s.split(' ');
-    let sum = arr.reduce((a,c) => {
-        if(c !== 'Z') return a + +c
-        else return a
-    }, 0)
-    for(let i=0; i<arr.length; i++) if(arr[i] === 'Z') sum -= arr[i-1]
-    
-    return sum
+    const stack = [];
+    s.split(' ').map(i => {
+        if(i === 'Z') stack.pop()
+        else stack.push(+i)
+    })
+    return stack.reduce((a,c) => a+c, 0)
 }
