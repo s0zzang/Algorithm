17fcdfea2a2a8ee5d1d83b7194531c9914dc3b0b
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-06-10 20:58:01 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1032 ms, Memory: 99692 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [N, M] = input.shift().split(" ").map(Number);
+
+const graph = {};
+for (let i = 1; i <= N; i++) graph[i] = [];
+for (const line of input) {
+  const [from, to] = line.split(" ").map(Number);
+  graph[from].push(to);
+  graph[to].push(from);
+}
+for (line in graph) graph[line].sort((a, b) => a - b);
+
+const visited = [];
+let count = 0;
+const dfs = (startV) => {
+  visited[startV] = true;
+  for (const el of graph[startV]) {
+    if (!visited[el]) dfs(el);
+  }
+};
+
+for (el in graph) {
+  if (!visited[el]) count++;
+  dfs(el);
+}
+
+console.log(count);
