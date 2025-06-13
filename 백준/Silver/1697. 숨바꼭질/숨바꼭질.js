82af588c9461cc4ba38f665f5d49cb141a50cb48
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-06-13 21:12:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 388 ms, Memory: 19896 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -8,26 +8,28 @@ const [from, to] = fs
   .map(Number);
 
 const move = (X) => [+X + 1, X - 1, X * 2];
+const totalTime = Array(100001).fill(0);
 
 // bfs로 풀기
 if (from === to) return console.log(0);
 const visited = new Set();
 
-const bfs = (X, time) => {
-  const q = [[X, time]];
+const bfs = (X) => {
+  const q = [X];
   visited.add(X);
 
   while (q.length) {
     const current = q.shift();
-    for (let next of move(current[0])) {
-      let [_, time] = current;
-      if (next === to) return console.log(time);
+    if (current === to) return totalTime[current];
+
+    for (let next of move(current)) {
       if (next < 0 || next > 100000) continue;
       if (visited.has(next)) continue;
-      q.push([next, ++time]);
+      q.push(next);
       visited.add(next);
+      totalTime[next] = totalTime[current] + 1;
     }
   }
 };
 
-bfs(from, 1);
+console.log(bfs(from));
