7718a8cf204bbf09c43d3294eaa8851115ed874d
이소정 <109408216+s0zzang@users.noreply.github.com>
2025-06-20 15:16:27 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 336 ms, Memory: 26316 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
@@ -4,11 +4,7 @@ const T = input.shift();
 
 for (let i = 0; i < T; i++) {
   const [N, M] = input.shift().split(" ").map(Number);
-  const arrA = input
-    .shift()
-    .split(" ")
-    .map(Number)
-    .sort((a, b) => a - b);
+  const arrA = input.shift().split(" ").map(Number);
   const arrB = input
     .shift()
     .split(" ")
@@ -16,11 +12,18 @@ for (let i = 0; i < T; i++) {
     .sort((a, b) => a - b);
 
   let result = 0;
+
   for (const a of arrA) {
-    if (a <= arrB[0]) continue;
-    for (const b of arrB) {
-      if (a > b) result++;
+    let left = 0;
+    let right = M - 1;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (a > arrB[mid]) left = mid + 1;
+      else right = mid - 1;
     }
+
+    result += left;
   }
 
   console.log(result);
